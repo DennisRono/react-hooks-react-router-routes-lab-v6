22ab147566b0f383b1db9bef5cef3f008dd56e77
Dennis Kibet <dennisrkibet@gmail.com>
2024-08-16 17:16:30 +0300

    completed the routing lab

--- src/pages/Movie.js
+++ src/pages/Movie.js
@@ -1,16 +1,32 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import NavBar from '../components/NavBar'
 
 function Movie() {
+  const { id } = useParams()
+  const [movie, setMovie] = useState(null)
+
+  useEffect(() => {
+    fetch(`http://localhost:4000/movies/${id}`)
+      .then((response) => response.json())
+      .then((data) => setMovie(data))
+  }, [id])
+
+  if (!movie) {
+    return <div>Loading...</div>
+  }
   return (
     <>
       <header>
-        {/* What component should go here? */}
+        <NavBar />
       </header>
       <main>
-        {/* Movie info here! */}
+        {movie.genres.map((genre, index) => (
+          <span key={index}>{genre}</span>
+        ))}
       </main>
     </>
-  );
-};
+  )
+}
 
-export default Movie;
+export default Movie
